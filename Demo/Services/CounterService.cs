691e116e12b87fd008b45f0d9930833435a17c7a
Stefan Jeglorz <S.Jeglorz@devware.de>
2024-07-09 15:26:14 +0200

    added demo application

---
+++ Demo/Services/CounterService.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo;
+
+public class CounterService
+{
+    public event EventHandler<int> OnCounterChanged;
+
+    private int count = 0;
+
+    public void Increase()
+    {
+        count++;
+        if (OnCounterChanged != null)
+        {
+            OnCounterChanged(this, count);
+        }
+    }
+}
